48a53ae29c26afd428664f2405758545a5bb585e
InfinitiesLoop <dareed@microsoft.com>
2010-04-27 13:53:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,120 @@
+A jQuery live bindings plugin - created for demonstration purposes.
+Adds 'attrChanging', 'attrChange', 'arrayChanging', 'arrayChange' events.
+Adds jQuery.x array operations: pop, push, splice, sort, reverse, shift, unshift
+Adds linkTo, linkFrom, and linkBoth plugins.
+
+'attrChange' event -- be notified when an attribute is changed.
+
+    function report(ev) {
+        alert("Change attr '" + ev.attrName + "' from '" +
+            ev.oldValue + "' to '" + ev.newValue + "'.");
+    }
+    
+    $("#el").attrChange(report)
+        // Change attr 'foo' from 'undefined' to 'bar'
+        .attr("foo", "bar").
+        // Change attr 'foo' from 'bar' to 'baz'
+        .attr("foo", "baz");
+        
+     // restricted scope
+     $("#el").attrChange("x", report)
+        // Change attr 'x' from 'undefined' to '1'
+        .attr( { x: "1", y: "2" } );
+        
+     $("#el").attrChange([ "x", "y" ], report)
+        // Change attr 'x' from 'undefined' to '1'
+        // Change attr 'y' from 'undefined' to '2'
+        .attr( { x: "1", y: "2", z: "3" } );
+       
+     // consolidating different mutation operations
+     // rather than a 'dataChange' event for data() operations
+     
+     $("#el").attrChange("data:foo", report)
+        // Change attr 'data:foo' from 'undefined' to 'bar'
+        .data( "foo", "bar" )
+        // Change attr 'data:!' from '[Object object]' to '[Object object]'
+        .data( { } );
+
+     // works with val()
+     $("#el").attrChange("val", report)
+        // Change attr 'val' from 'hi' to 'bye'
+        .val( 'bye' );
+
+'attrChanging' event -- be notified before an attribute changes
+
+    $("#el")
+        .attrChanging(function(ev) {
+            if (!confirm("Allow changing attr '" + ev.attrName + "' from '" +
+                ev.oldValue + "' to '" + ev.newValue + "'?")) {
+                
+                ev.preventDefault();
+            }
+            
+        })
+        // Allow changing attr 'y' from 'undefined' to '2'?
+        // yes: value set, attrChange event raised
+        // no: value not set, attrChange event not raised
+        .attr("foo", "bar");
+        
+'arrayChange' event -- be notified when an array is modified
+
+    var arr = [1,2,3];
+    $([arr])
+        .arrayChange(function(ev) {
+            alert("Array operation " + ev.change + " executed with args " + ev.arguments);
+        });
+    // Array operation push executed with args 4,5
+    $.push(arr, 4, 5);
+    
+    // restricted scope
+    $("push", [arr])
+        .arrayChange(function(ev) {
+            alert("Array operation " + ev.change + " executed with args " + ev.arguments);
+        });
+    // nothing
+    $.pop(arr);
+    // Array operation push executed with args 4,5
+    $.push(arr, 4, 5);
+    
+    $(["push", "pop", "splice"], [arr])
+        .arrayChange(function(ev) {
+            alert("Array operation " + ev.change + " executed with args " + ev.arguments);
+        });
+    // Array operation pop executed with args undefined
+    $.pop(arr);
+    // Array operation push executed with args 4,5
+    $.push(arr, 4, 5);
+    // nothing
+    $.splice(arr, 0, 1);
+    
+'arrayChanging' event -- be notified before an array is modified
+    // works just like 'attrChange', can cancel operation
+    
+'linkTo' plugin -- bind the value of the current object to the value of another
+
+    var person = {};
+    $("#name").linkTo("val", person, "name");
+    alert(person.name); // $("#name").val()
+    $("#name").val("foo");
+    alert(person.name); // foo
+    // ... user changes value ...
+    alert(person.name); // <user typed value>
+    
+'linkFrom' plugin -- bind the value of the current object from the value of another
+
+    var person = {};
+    $(person).linkFrom("name", "#name", "val");
+    alert(person.name); // $("#name").val()
+    $("#name").val("foo");
+    alert(person.name); // foo
+    // ... user changes value ...
+    alert(person.name); // <user typed value>
+
+'linkBoth' plugin -- links the values of two objects to each other
+    var person = {};
+    $(person).linkBoth("name", "#name", "val");
+    alert(person.name); // $("#name").val()
+    $("#name").val("foo");
+    alert(person.name); // foo
+    $(person).attr("name", "bob");
+    alert($("#name").val()); // bob
---
+++ jQuery.livelink.js
@@ -0,0 +1,244 @@
+/*
+ * jQuery live links plugin
+ * http://github.com/nje
+ */
+(function($){
+
+var oldattr = $.attr,
+    oldval = $.fn.val;
+    olddata = $.data;
+
+function attr( obj, name, value, pass ) {
+    // an attr that supports plain objects (won't look for .style, nodeType, etc)
+    return $.isPlainObject( obj )
+        ? ( value === undefined ? obj[ name ] : ( obj[ name ] = value ) )
+        : oldattr( obj, name, value, pass );
+}
+
+function raiseEvents(type, context, change, setValue) {
+    // todo: peek if there are any listeners to avoid extra work
+    var ret,
+        event = $.Event( type + "Changing" ),
+        isArray =  type === "array";
+    event.newValue = isArray ? change.arguments : change.newValue;
+    $.event.trigger( event, [ change ], context );
+    if (!event.isDefaultPrevented()) {
+        var newvalue = isArray ? change.arguments : event.newValue;
+        ret = setValue(newvalue);
+        var oldvalue = change.oldValue;
+        if ( isArray || typeof oldvalue === "undefined" || newvalue !== oldvalue ) {
+            isArray ? (change.arguments = newvalue) : (change.newValue = newvalue);
+            $.event.trigger( type + "Change", [ change ], context );
+        }
+    }
+    return ret;
+}
+
+$.attr = function( elem, name, value, pass ) {
+    var ret = this;
+    if (value === undefined) 
+        ret = attr( elem, name, value, pass );
+    else {
+        raiseEvents( "attr", elem, { attrName: name, oldValue: attr( elem, name ), newValue: value },
+            function(newvalue) {
+                attr( elem, name, value, pass );
+            });
+    }
+    return ret;
+}
+
+$.fn.val = function( value ) {
+    var ret = this;
+    if ( value === undefined )
+        ret = oldval.call( this );
+    else {
+        this.each(function() {
+            var self = $(this);
+            raiseEvents( "attr", this, { attrName: "val", oldValue: oldval.call( self ), newValue: value },
+                function(newvalue) {
+                    oldval.call( self, newvalue );
+                });
+        });
+    }
+    return ret;
+}
+
+$.data = function( elem, name, data ) {
+    var ret;
+    if ( typeof data === "undefined" ) {
+        ret = olddata( elem, name );
+    }
+    else {
+        var oldvalue,
+            newvalue = data,
+            attrName;
+        if ( typeof name === "object" ) {
+            attrName = "data:!";
+            newvalue = name;
+        }
+        else {
+            attrName = "data:" + name;
+            oldvalue = olddata( elem, name );
+        }
+        ret = raiseEvents( "attr", elem, { attrName: attrName, oldValue: oldvalue, newValue: newvalue },
+            function(newvalue) {
+                return olddata( elem, name, newvalue );
+            });
+    }
+    return ret;
+}
+
+$.each( "pop push reverse shift sort splice unshift".split(" "), function( i, name ) {
+    $[ name ] = function( arr ) {
+        var args = $.makeArray( arguments );
+        args.splice( 0, 1 );
+        return raiseEvents( "array", arr, { change: name, arguments: args }, function(arguments) {
+            arr[ name ].apply( arr, arguments );
+        });
+    }
+});
+
+var special = $.event.special,
+    formElems = /textarea|input|select/i;
+
+$.each( ["attrChanging", "attrChange", "arrayChanging", "arrayChange"], function( i, name ) {
+    var isattr = i < 2;
+    
+    $.fn[ name ] = function(filter, fn) {
+        if ( arguments.length === 1 ) {
+            fn = filter;
+            filter = null;
+        }
+        return fn ? this.bind( name, filter, fn ) : this.trigger( name );
+    }
+    
+    special[ name ] = {
+        add: function( handleObj ) {
+            var old_handler = handleObj.handler;
+            handleObj.handler = function( event, change ) {
+                var data = handleObj.data,
+                    attrName = isattr ? change.attrName : change.change;
+                if ( !data || data === attrName || $.inArray( attrName, data ) > -1 ) {
+                    $.extend( event, change );
+                    // todo: support extra parameters passed to trigger as
+                    // trigger('attrChange', [<change>, extra1, extra2]).
+                    old_handler.call( this, event );
+                }
+            }
+        }
+    }
+});
+$.extend(special.attrChange, {
+        setup: function() {
+            // when a form element's val() changes, it raises the attrChange
+            // event where attrName is "val". There is no attrChanging event
+            // in this case.
+            if ( formElems.test( this.nodeName ) ) {
+                $(this).bind( "change.attrChange", function(ev) {
+                    var self = $( this );
+                    self.trigger( "attrChange", { attrName: "val", newValue: self.val() } );
+                } );
+            }
+        },
+        teardown: function() {
+            $(this).unbind( "change.attrChange" );
+        }
+});
+
+// "live" link bindings
+
+var setter_lookup = {
+    val: "val",
+    html: "html",
+    text: "text"
+}
+
+$.link = function( settings ) {
+    var source = settings.source,
+        target = settings.target,
+        sourceAttr = settings.sourceAttr,
+        targetAttr = settings.targetAttr,
+        convert = settings.convert;
+    // wrap arrays in another array because $([]) is treatment of
+    // the contents, not the array itself
+    source = $($.isArray( source ) ? [ source ] : source);
+    target = $($.isArray( target ) ? [ target ] : target);
+    convert = $.fn.convertFn[ convert ] || convert;
+    var isVal = sourceAttr === "val",
+        targetFn = setter_lookup[targetAttr];
+    function update(ev) {
+        var newValue;
+        if ( ev ) {
+            newValue = ev.newValue;
+        }
+        else if ( sourceAttr.indexOf( "data:" ) === 0 ) {
+            newValue = source.data( sourceAttr.substr( 5 ) );
+        }
+        else {
+            newValue = source.attr( sourceAttr );
+        }
+        var newValue = ev ? ev.newValue : ( isVal ? source.val() : source.attr( sourceAttr ) );
+        if ( convert ) {
+            newValue = convert( newValue, settings );
+        }
+        if ( newValue !== undefined ) {
+            if (targetFn) {
+                target[ targetFn ].call( target, newValue );
+            }
+            else if ( targetAttr.indexOf( "data:" ) === 0 ) {
+                target.data( targetAttr.substr( 5 ), newValue );
+            }
+            else {
+                target.attr( targetAttr, newValue );
+            }
+        }
+    }
+    source.attrChange( sourceAttr, update );
+    // force an update immediately, before the first change
+    update();
+}
+
+$.fn.extend({
+    convertFn: {
+        "!": function() {
+        }
+    },
+    linkFrom: function( targetAttr, source, sourceAttr, convert ) {
+        var settings = {
+            target: this
+        };
+        if ( $.isPlainObject( targetAttr ) ) {
+            $.extend( settings, targetAttr );
+        }
+        else {
+            settings.source = source;
+            settings.sourceAttr = sourceAttr;
+            settings.targetAttr = targetAttr;
+            settings.convert = convert;
+        }
+        $.link( settings );
+        return this;
+    },
+    linkTo: function( sourceAttr, target, targetAttr, convert ) {
+        var settings = {
+            source: this
+        };
+        if ( $.isPlainObject( sourceAttr ) ) {
+            $.extend( settings, sourceAttr );
+        }
+        else {
+            settings.target = target;
+            settings.targetAttr = targetAttr;
+            settings.sourceAttr = sourceAttr;
+            settings.convert = convert;
+        }
+        $.link( settings );
+        return this;
+    },
+    linkBoth: function( targetAttr, source, sourceAttr ) {
+        return this.linkTo( targetAttr, source, sourceAttr ).
+            linkFrom( targetAttr, source, sourceAttr );
+    }
+});
+
+})(jQuery);
