8e53293f66d6d4a50202ebe44d4cc6ff96f4b419
BorisMoore <BorisMoore@gmail.com>
2010-10-11 18:07:38 -0700

    Removed the out-of-date version of jquery.tmpl. Updated demo to use the jquery.tmpl beta here: http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.js. Moved the demo into a subfolder. Fixed bug which caused error when adding new contact.

--- demos/demo-contacts.js
+++ demos/demo-contacts.js
@@ -1,4 +1,4 @@
-jQuery(function($){
+jQuery( function( $ ){
 
 // define some basic default data to start with
 var contacts = [
@@ -10,39 +10,36 @@ var contacts = [
         { type: "Home", number: "(555) 999-1212" } ]  }
 ];
 
-// enable using 'contacts' as the name of the template
-$.templates.contacts = $.tmpl($("#contacttmpl").html());
-
-$.extend($.convertFn, {
+$.extend( $.convertFn, {
     // linking converter that normalizes phone numbers
-    phone: function(value) {// turn a string phone number into a normalized one with dashes
+    phone: function( value ) {// turn a string phone number into a normalized one with dashes
         // and parens
-        value = (parseInt(value.replace(/[\(\)\- ]/g, ""), 10) || 0).toString();
+        value = (parseInt( value.replace( /[\(\)\- ]/g, "" ), 10 ) || 0 ).toString();
         value = "0000000000" + value;
-        value = value.substr(value.length - 10);
-        value = "(" + value.substr(0,3) + ") " + value.substr(3,3) + "-" + value.substr(6);
+        value = value.substr( value.length - 10 );
+        value = "(" + value.substr( 0, 3 ) + ") " + value.substr( 3, 3 ) + "-" + value.substr( 6 );
         return value;
     },
-    fullname: function(value, source, target) {
+    fullname: function( value, source, target ) {
         return source.firstName + " " + source.lastName;
     }
 });
 
 // show the results of the linking -- object graph is already full of the data
-$("#save").click(function() {
-    $("#results").html(JSON.stringify(contacts, null, 4));
+$( "#save" ).click( function() {
+    $( "#results" ).html( JSON.stringify( contacts, null, 4 ));
 });
 
 // add a new contact when clicking the insert button.
 // notice that no code here exists that explicitly redraws
 // the template.
-$("#insert").click(function() {
-    contacts.push({ firstName: "", lastName: "", phones: [] });
+$( "#insert" ).click( function() {
+    contacts.push({ firstName: "first", lastName: "last", phones: [], age:20 });
     refresh();
 });
 
-$("#sort").click(function() {
-    contacts.sort(function(a,b) {
+$( "#sort" ).click( function() {
+    contacts.sort( function( a, b ) {
         return a.lastName < b.lastName ? -1 : 1;
     });
     refresh();
@@ -51,55 +48,56 @@ $("#sort").click(function() {
 // function that clears the current template and renders it with the
 // current state of the global contacts variable.
 function refresh() {
-    $(".contacts").empty().append("contacts", {contacts:contacts});
+    $( ".contacts" ).empty();
+	$( "#contacttmpl" ).tmpl( {contacts:contacts} ).appendTo( ".contacts" );
     // bind inputs to the data items
-    $("tr.contact").each(function(i) {
+    $( "tr.contact" ).each( function(i) {
         var contact = contacts[i];
-        $("input.contact", this).link(contact);
-        $('.agebar', this).link(contact, {
+        $( "input.contact", this ).link( contact );
+        $( '.agebar', this ).link( contact, {
 			age: {
-				convertBack: function(value, source, target) {
-					$(target).width(value + "px");
+				convertBack: function( value, source, target ) {
+					$( target ).width( value + "px" );
 				}
 			}
 		});
-		$(contact).trigger("changeData", ["age", contact.age]);
+		$( contact ).trigger( "changeData", ["age", contact.age] );
 		
         // todo: "update" feature
         
-        $(".contact-remove", this).click(function() {
-            contacts.splice(i, 1);
+        $( ".contact-remove", this ).click( function() {
+            contacts.splice( i, 1 );
             refresh();
         });
         var original_firstName = contact.firstName,
             original_lastName = contact.lastName;
-        $(".contact-reset", this).click(function() {
-            $(contact)
-                .data("firstName", original_firstName)
-                .data("lastName", original_lastName);
+        $( ".contact-reset", this ).click( function() {
+            $( contact )
+                .data( "firstName", original_firstName )
+                .data( "lastName", original_lastName );
         });
         
-        $("tr.phone", this).each(function(i) {
+        $( "tr.phone", this ).each( function(i) {
             var phone = contact.phones[i];
-            $(this).link(phone, {
+            $( this ).link( phone, {
                 type: "type",
                 number: {
                     name: "number",
                     convert: "phone"
                 }
             });
-            $(".phone-remove", this).click(function() {
+            $( ".phone-remove", this ).click( function() {
                 // note: I'd like to only redraw the phones portion of the
                 // template, but jquery.tmpl.js does not support nested templates
                 // very easily. So here I am triggering an arrayChange event on
                 // the main contacts array to force the entire thing to refresh.
                 // Note that user input is not lost since the live linking has
                 // already stored the values in the object graph.
-                contact.phones.splice(i, 1);
+                contact.phones.splice( i, 1 );
                 refresh();
             });
         });
-        $(".newphone", this).click(function() {
+        $( ".newphone", this ).click( function() {
             contact.phones.push({ type: "", number: "" });
             refresh();
         });
